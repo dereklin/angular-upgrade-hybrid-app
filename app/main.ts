e69abd7cdcf300b990a622dfdacb3f482afa1c39
Derek Lin <Derek Lin>
2016-09-04 15:16:34 -0700

    angular 1 and 2 hybrid app

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,19 @@
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// import { AppModule } from './app.module';
+
+// platformBrowserDynamic().bootstrapModule(AppModule);
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app.module';
+declare var angular : any;
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+import { UpgradeAdapter } from '@angular/upgrade';
+
+let upgradeAdapter = new UpgradeAdapter(AppModule);
+
+angular.module('ng1module', []);
+
+upgradeAdapter.bootstrap(document.documentElement, ['ng1module']);
