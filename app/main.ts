b7740703fada2c687924e0224712b10cb072ff9d
Derek Lin <Derek Lin>
2016-09-04 15:22:33 -0700

    clean up

--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,6 @@
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-// import { AppModule } from './app.module';
-
-// platformBrowserDynamic().bootstrapModule(AppModule);
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
-declare var angular : any;
-
-
-
 import { UpgradeAdapter } from '@angular/upgrade';
-
+declare var angular : any;
 let upgradeAdapter = new UpgradeAdapter(AppModule);
-
 angular.module('ng1module', []);
-
 upgradeAdapter.bootstrap(document.documentElement, ['ng1module']);
